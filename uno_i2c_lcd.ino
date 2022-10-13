74794fcbb75a51ecbe79212dfcbe69f77ee4107f
Ali Fainur Zakira <58428727+alifainur@users.noreply.github.com>
2022-10-13 12:06:22 +0800

    Update uno_i2c_lcd.ino

--- uno_i2c_lcd.ino
+++ uno_i2c_lcd.ino
@@ -6,11 +6,11 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup()
 {
 	lcd.begin();
+	lcd.backlight();
 }
 
 void loop()
 {
-	lcd.backlight();
 	lcd.clear();
 	lcd.setCursor(4,0);
 	lcd.print("Ali Fainur");  
